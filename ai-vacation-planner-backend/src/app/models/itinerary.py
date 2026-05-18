ee7eb3d7994e09bf808b1f7fb19908133553bb88
Guhirwa <christianguhirwa74@gmail.com>
2026-05-18 23:51:29 +0200

    fix(itinerary-model): add the missing method invication parameter to the method func.now - fix the error on line 13 and 14 for the missing ()

--- ai-vacation-planner-backend/src/app/models/itinerary.py
+++ ai-vacation-planner-backend/src/app/models/itinerary.py
@@ -10,7 +10,7 @@ class Itinerary(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     trip_id = Column(Integer, ForeignKey("trips.id", ondelete="CASCADE"))
     days_data = Column(JSON, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now)
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
     trip = relationship("Trip", back_populates="itinerary", cascade="all, delete-orphan")
