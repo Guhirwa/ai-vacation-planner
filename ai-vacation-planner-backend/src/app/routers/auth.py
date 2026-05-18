eb8d070a3d3057139272a2ea9cd88bf0d0e0d095
Guhirwa <christianguhirwa74@gmail.com>
2026-05-18 11:23:22 +0200

    feat(app-routes): declare and implement register endpoint - standard responce model of UserOut and override the default status code of 200 OK to 201 CREATED - validate request payload with UserCreate pydantic model and receive the database Session - first verify if the user with the same email or username exit and raise error to avoid data duplicate in adatabase - if not create new user model, populate the data includeing the hashed password, commit, refresh data and return new added user object

--- ai-vacation-planner-backend/src/app/routers/auth.py
+++ ai-vacation-planner-backend/src/app/routers/auth.py
@@ -1,3 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_database
+from app.models import User
+from app.schemas.auth import UserOut, UserCreate
+from app.utils.security import get_password_hash
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+@router.post("/register", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+def register(user: UserCreate, database: Session = Depends(get_database())):
+    existing_user = database.query(User).filter(
+        (User.email == user.email) | (User.username == user.username)
+    ).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email or Username already registered")
+
+    hashed_password = get_password_hash(user.password)
+    database_user = User(email=user.email, username=user.username, hashed_password=hashed_password)
+    database.add(database_user)
+    database.commit()
+    database.refresh(database_user)
+    return database_user
