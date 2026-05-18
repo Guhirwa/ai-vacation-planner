9242a84d219f70fd03408c67c2419394bf10a947
Guhirwa <christianguhirwa74@gmail.com>
2026-05-18 10:04:51 +0200

    feat(auth-dependencies): declare the function that will be used to protect routes - credentials depending to the security will extract the token from Authorization header - database depending to the get_database function from database configuration file will provide database session

--- ai-vacation-planner-backend/src/app/dependencies/auth.py
+++ ai-vacation-planner-backend/src/app/dependencies/auth.py
@@ -1,3 +1,12 @@
-from fastapi.security import HTTPBearer
+from fastapi import Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
+from app.database import get_database
 
 security = HTTPBearer
+
+def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Depends(security),
+        database: Session = Depends(get_database())
+):
+    
