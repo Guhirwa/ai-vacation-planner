c358695134b4de4206f9f7540f5127469332985a
Guhirwa <christianguhirwa74@gmail.com>
2026-05-18 10:24:18 +0200

    feat(auth-dependencies): implementation of get_current_user cont' - open try and catch for sensitive operation to be able to handle or success and error results - decode and verify the JWT Token. the secret code will be loade all the way from .env - jwt.decode, checks if signature is valid or token not tampered, if token hasn't expired and if algorithm matches - extract the sub from the payload and get user_id. - by user_id I will be able to check if user exist or not and fro any error raise the custom exception created

--- ai-vacation-planner-backend/src/app/dependencies/auth.py
+++ ai-vacation-planner-backend/src/app/dependencies/auth.py
@@ -1,8 +1,11 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
+from app.config import settings
 from app.database import get_database
+from app.models import User
 
 security = HTTPBearer
 
@@ -16,3 +19,15 @@ def get_current_user(
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"}
     )
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        user_id: int = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = database.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
